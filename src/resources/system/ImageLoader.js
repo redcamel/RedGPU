5a206c77d8732fa210c5ea6016515e065565bbb4
백선기 <webseon@gmail.com>
2019-12-20 22:11:00 +0900

    #156 흠

--- src/resources/system/ImageLoader.js
+++ src/resources/system/ImageLoader.js
@@ -2,7 +2,7 @@
  *   RedGPU - MIT License
  *   Copyright (c) 2019 ~ By RedCamel( webseon@gmail.com )
  *   issue : https://github.com/redcamel/RedGPU/issues
- *   Last modification time of this file - 2019.12.20 20:37:20
+ *   Last modification time of this file - 2019.12.20 22:10:59
  *
  */
 
@@ -107,12 +107,6 @@ const worker = createWorker(() => {
 		})
 
 	});
-});
-window.addEventListener("beforeunload", function (event) {
-	event.preventDefault();
-	worker.terminate()
-	// event.returnValue = 'alert(\' dhsi\')';
-
 });
 function loadImageWithWorker(src) {
 	return new Promise((resolve, reject) => {
