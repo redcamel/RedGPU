e9ca480dd69d2fce5f67c66feaa3428688eb737f
psk <psk@midasit.com>
2020-03-24 21:21:30 +0900

    glslang 0.15

--- testRnd/textTest.js
+++ testRnd/textTest.js
@@ -2,61 +2,124 @@
  *   RedGPU - MIT License
  *   Copyright (c) 2019 ~ By RedCamel( webseon@gmail.com )
  *   issue : https://github.com/redcamel/RedGPU/issues
- *   Last modification time of this file - 2020.1.14 17:51:9
+ *   Last modification time of this file - 2020.3.24 21:21:28
  *
  */
-import RedGPU from "../src/RedGPU.js";
+import RedGPU from "https://redcamel.github.io/RedGPU/src/RedGPU.js";
 
 const cvs = document.createElement('canvas');
 document.body.appendChild(cvs);
 
 
 new RedGPU.RedGPUContext(cvs,
-	function (v, reason) {
-		console.log(this.context)
-		let tView;
+	function () {
 		let tScene = new RedGPU.Scene();
-		let tGrid = new RedGPU.Grid(this)
-		let tCamera = new RedGPU.ObitController(this)
-		let tLight
-		tScene.backgroundColor = '#fff'
-		tLight = new RedGPU.DirectionalLight(this)
-		tLight.x = 0
-		tLight.y = 100
-		tLight.z = 0
-		tScene.addLight(tLight)
-		// tScene.axis = new RedGPU.Axis(this)
-		tView = new RedGPU.View(this, tScene, tCamera)
-		tCamera.targetView = tView // optional
-		tScene.grid = tGrid
+		tScene.backgroundColor = '#fff';
 
-		this.addView(tView)
+		let tCamera = new RedGPU.ObitController(this);
+		let tView = new RedGPU.View(this, tScene, tCamera);
+		this.addView(tView);
+		tCamera.distance = 2;
 
+		this.setSize(window.innerWidth, window.innerHeight);
 
-		let i = 1
-		let tMesh
-		while (i--) {
-			 tMesh = new RedGPU.Text(this,512,512)
-			tMesh.fontSize = 36
-			tMesh.text = i%2 ? '가나다라마바사' : 'ABCDEFG'
-			tScene.addChild(tMesh)
+		// Cube data
+		//             1.0 y
+		//              ^  -1.0
+		//              | / z
+		//              |/       x
+		// -1.0 -----------------> +1.0
+		//            / |
+		//      +1.0 /  |
+		//           -1.0
+		//
+		//         [7]------[6]
+		//        / |      / |
+		//      [3]------[2] |
+		//       |  |     |  |
+		//       | [4]----|-[5]
+		//       |/       |/
+		//      [0]------[1]
+		//
+		let interleaveData = new Float32Array(
+			[
+				// Front face
+				-0.5, -0.5,  0.5,    0.0,  0.0,  1.0,   0.0, 0.0, // v0
+				0.5, -0.5,  0.5,    0.0,  0.0,  1.0,   1.0, 0.0, // v1
+				0.5,  0.5,  0.5,    0.0,  0.0,  1.0,   1.0, 1.0, // v2
+				-0.5,  0.5,  0.5,    0.0,  0.0,  1.0,   0.0, 1.0, // v3
+				// Back face
+				-0.5, -0.5, -0.5,    0.0,  0.0, -1.0,   1.0, 0.0, // v4
+				0.5, -0.5, -0.5,    0.0,  0.0, -1.0,   1.0, 1.0, // v5
+				0.5,  0.5, -0.5,    0.0,  0.0, -1.0,   0.0, 1.0, // v6
+				-0.5,  0.5, -0.5,    0.0,  0.0, -1.0,   0.0, 0.0, // v7
+				// Top face
+				0.5,  0.5,  0.5,    0.0,  1.0,  0.0,   0.0, 1.0, // v2
+				-0.5,  0.5,  0.5,    0.0,  1.0,  0.0,   0.0, 0.0, // v3
+				-0.5,  0.5, -0.5,    0.0,  1.0,  0.0,   1.0, 0.0, // v7
+				0.5,  0.5, -0.5,    0.0,  1.0,  0.0,   1.0, 1.0, // v6
+				// Bottom face
+				-0.5, -0.5,  0.5,    0.0,  1.5,  0.0,   1.0, 1.0, // v0
+				0.5, -0.5,  0.5,    0.0,  1.5,  0.0,   0.0, 1.0, // v1
+				0.5, -0.5, -0.5,    0.0,  1.5,  0.0,   0.0, 0.0, // v5
+				-0.5, -0.5, -0.5,    0.0,  1.5,  0.0,   1.0, 0.0, // v4
+				// Right face
+				0.5, -0.5,  0.5,    1.0,  0.0,  0.0,   1.0, 0.0, // v1
+				0.5,  0.5,  0.5,    1.0,  0.0,  0.0,   1.0, 1.0, // v2
+				0.5,  0.5, -0.5,    1.0,  0.0,  0.0,   0.0, 1.0, // v6
+				0.5, -0.5, -0.5,    1.0,  0.0,  0.0,   0.0, 0.0, // v5
+				// Left face
+				-0.5, -0.5,  0.5,   -1.0,  0.0,  0.0,   0.0, 0.0, // v0
+				-0.5,  0.5,  0.5,   -1.0,  0.0,  0.0,   1.0, 0.0, // v3
+				-0.5,  0.5, -0.5,   -1.0,  0.0,  0.0,   1.0, 1.0, // v7
+				-0.5, -0.5, -0.5,   -1.0,  0.0,  0.0,   0.0, 1.0  // v4
+			]
+		);
+		let indexData = new Uint16Array(
+			[
+				0,  1,  2,    0,  2 , 3,  // Front face
+				4,  5,  6,    4,  6 , 7,  // Back face
+				8,  9, 10,    8, 10, 11,  // Top face
+				12, 13, 14,   12, 14, 15,  // Bottom face
+				16, 17, 18,   16, 18, 19,  // Right face
+				20, 21, 22,   20, 22, 23   // Left face
+			]
+		);
 
-		}
-		tMesh.scaleX = 10
-		tMesh.scaleY = 10
-
+		let geometry = new RedGPU.Geometry(
+			this,
+			new RedGPU.Buffer(
+				this,
+				'interleaveBuffer',
+				RedGPU.Buffer.TYPE_VERTEX,
+				new Float32Array(interleaveData),
+				[
+					new RedGPU.InterleaveInfo('vertexPosition', 'float3'),
+					new RedGPU.InterleaveInfo('vertexNormal', 'float3'),
+					new RedGPU.InterleaveInfo('texcoord', 'float2')
+				]
+			),
+			new RedGPU.Buffer(
+				this,
+				'indexBuffer',
+				RedGPU.Buffer.TYPE_INDEX,
+				new Uint32Array(indexData)
+			)
+		);
+		let texture = new RedGPU.BitmapTexture(this, 'https://cx20.github.io/webgpu-test/assets/textures/frog.jpg');
+		let textureMat = new RedGPU.BitmapMaterial(this, texture);
+		let tMesh = new RedGPU.Mesh(this, geometry, textureMat);
+		tMesh.cullMode = 'none';
+		tScene.addChild(tMesh);
 
 		let renderer = new RedGPU.Render();
-		let render = time => {
-			tScene._children.forEach(tMesh => {
-
-				// tMesh.material.alpha = RedGPU.UTIL.clamp(Math.sin(time / 500), 0, 1)
-
-			})
-			// console.log(tCamera.getPosition())
+		let render =  (time)=> {
+			tMesh.rotationX += 1;
+			tMesh.rotationY += 1;
+			tMesh.rotationZ += 1;
 			renderer.render(time, this);
 			requestAnimationFrame(render);
-		}
+		};
 		requestAnimationFrame(render);
 
 
