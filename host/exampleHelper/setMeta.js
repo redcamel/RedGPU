57850cc96c37e1f8ccffe419b19b316569fa79fb
webseon@gmail.com <dreamFActory3!>
2023-02-07 12:09:42 +0900

    #332

--- host/exampleHelper/setMeta.js
+++ host/exampleHelper/setMeta.js
@@ -1,7 +1,7 @@
 const setMeta = () => {
 	[
-		'AuT39+Duz+ZRtx1SlkTa2EJAL3fE4ffZq72D+4KnjZkx0XC9g6GL+kteswN1p9S6Kd4NJUlAwMWB1VWwr3lCqAgAAABleyJvcmlnaW4iOiJodHRwczovL3JlZGNhbWVsLmdpdGh1Yi5pbzo0NDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjc1MjA5NTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=',
-		'AoObp2NQ8sEb9+TWvbU/cWFBaNRNGXZtCp6yZSrjuqlH+JDRV9cP+abD9k2ydtE92JpehZbovmhZ2pKiG8obLAYAAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjc1MjA5NTk5fQ=='
+		'Aq5SYGQ4/wmH5H+FkvfSKNla9anTUSPcha0K04RTnnLIjD/wikUsWb2jVYv5gt/kzNVvhDCUa32vyx0TztVx5wkAAABleyJvcmlnaW4iOiJodHRwczovL3JlZGNhbWVsLmdpdGh1Yi5pbzo0NDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjkxNzExOTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=',
+		'AqzyRNDDUH7C6OOrJRvTouUcm8m2n/t9i1LYLBWEZTKZbsflEYhQjXYFP35HhHmdaf6MWbF0nvrEiJ+ueQ7rEw4AAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjkxNzExOTk5fQ=='
 	].forEach(v => {
 		const t0 = document.createElement('meta')
 		t0.setAttribute('http-equiv', "origin-trial")
