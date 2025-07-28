00b93cf91a56bb07d3ab4cff51f98cd5a909ec72
redcamel <webseon@gmail.com>
2025-07-28 18:09:04 +0900

    Refactor `createBasePipeline` to use `resourceManager` for bind group layout and pipeline layout creation.

--- src/display/mesh/core/pipeline/createBasePipeline.ts
+++ src/display/mesh/core/pipeline/createBasePipeline.ts
@@ -2,6 +2,7 @@ import GPU_COMPARE_FUNCTION from "../../../../gpuConst/GPU_COMPARE_FUNCTION";
 import ResourceManager from "../../../../resources/resourceManager/ResourceManager";
 import Mesh from "../../Mesh";
 import PIPELINE_TYPE from "./PIPELINE_TYPE";
+import resourceManager from "../../../../resources/resourceManager/ResourceManager";
 
 const createBasePipeline = (
 	mesh: Mesh,
@@ -11,7 +12,7 @@ const createBasePipeline = (
 ): GPURenderPipeline => {
 	// 메쉬에서 GPU 리소스 관리자와 GPU 디바이스를 가져옵니다.
 	const {redGPUContext} = mesh
-	const {gpuDevice} = redGPUContext
+	const {gpuDevice,resourceManager} = redGPUContext
 	const material_gpuRenderInfo = mesh.material.gpuRenderInfo
 	let entryPoint: string;
 	let pipelineLabel: string;
@@ -37,12 +38,12 @@ const createBasePipeline = (
 	}
 	// BindGroup 레이아웃을 가져온다
 	const bindGroupLayouts: GPUBindGroupLayout[] = [
-		redGPUContext.resourceManager.getGPUBindGroupLayout(ResourceManager.PRESET_GPUBindGroupLayout_System),
+		resourceManager.getGPUBindGroupLayout(ResourceManager.PRESET_GPUBindGroupLayout_System),
 		vertexBindGroupLayout
 	]
 	if (pipelineType !== PIPELINE_TYPE.SHADOW) bindGroupLayouts.push(material_gpuRenderInfo.fragmentBindGroupLayout)
-	const pipelineLayoutDescriptor: GPUPipelineLayoutDescriptor = {bindGroupLayouts, label: `${module.label}_pipelineLayout`}
-	const pipelineLayout: GPUPipelineLayout = gpuDevice.createPipelineLayout(pipelineLayoutDescriptor);
+	const pipelineLayoutDescriptor: GPUPipelineLayoutDescriptor = {bindGroupLayouts}
+	const pipelineLayout: GPUPipelineLayout = resourceManager.createGPUPipelineLayout(`${module.label}_pipelineLayout`,pipelineLayoutDescriptor);
 	const pipelineDescriptor: GPURenderPipelineDescriptor = {
 		label: pipelineLabel,
 		layout: pipelineLayout,
