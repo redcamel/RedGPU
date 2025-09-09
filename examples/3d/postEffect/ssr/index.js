bb25f13af22f01a5db69a3bf11c360f3b696aa93
webseon <webseon@gmail.com>
2025-09-09 16:36:25 +0900

    PhongMaterial: Added roughness parameter and updated SSR reflection logic. Updated examples with new roughness property integration.

--- examples/3d/postEffect/ssr/index.js
+++ examples/3d/postEffect/ssr/index.js
@@ -45,23 +45,10 @@ RedGPU.init(canvas, (redGPUContext) => {
 
 	scene.addChild(metalSphere);
 
-	{
-		const sphereGeometry = new RedGPU.Primitive.Ground(redGPUContext,50,50);
-		const metalMaterial = new RedGPU.Material.PhongMaterial(redGPUContext);
-		metalMaterial.color.setColorByHEX('#00ff00');
-		metalMaterial.useSSR = true;
-		metalMaterial.metallic = 0.5;
-		const metalSphere = new RedGPU.Display.Mesh(redGPUContext, sphereGeometry, metalMaterial);
-		metalSphere.y = -1;
-
-		scene.addChild(metalSphere);
-	}
-
 	// GLTF 모델 로딩
 	const gltfModels = [
 		{ url: 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/main/Models/DamagedHelmet/glTF-Binary/DamagedHelmet.glb', position: {} },
 		{ url: 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/main/Models/MetalRoughSpheres/glTF-Binary/MetalRoughSpheres.glb', position: { z: -2 } },
-		{ url: '../../../assets/gltf/busterDrone/busterDrone.gltf', position: { x: 2,} },
 
 	];
 
