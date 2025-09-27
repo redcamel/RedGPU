7d36a935d5a2aee1219d2ef4224f05f4192a8171
webseon <webseon@gmail.com>
2025-09-27 16:43:03 +0900

    Docs - lights

--- src/light/lights/DirectionalLight.ts
+++ src/light/lights/DirectionalLight.ts
@@ -1,49 +1,81 @@
 import ColorRGB from "../../color/ColorRGB";
 import convertHexToRgb from "../../utils/convertColor/convertHexToRgb";
 import BaseLight from "../core/BaseLight";
+
 /**
- * <iframe src="/RedGPU/examples/3d/light/directionalLight/"></iframe>
+ * 방향성 광원을 정의하는 클래스입니다.
+ *
+ * 이 광원은 특정 방향으로 균일하게 빛을 투사하며, 태양광과 같은 효과를 구현할 때 사용됩니다.
+ *
+ * @example
+ * ```javascript
+ * const light = new RedGPU.Light.DirectionalLight();
+ * scene.lightManager.addDirectionalLight(light);
+ * ```
+ *
+ * <iframe src="/RedGPU/examples/3d/light/directionalLight/" width="100%" height="400" style="border:none;"></iframe>
  */
 class DirectionalLight extends BaseLight {
-	#directionX: number = -1
-	#directionY: number = -1
-	#directionZ: number = -1
+	/** 광원의 X 방향 벡터 값 */
+	#directionX: number = -1;
+	/** 광원의 Y 방향 벡터 값 */
+	#directionY: number = -1;
+	/** 광원의 Z 방향 벡터 값 */
+	#directionZ: number = -1;
 
+	/**
+	 * 새로운 DirectionalLight 인스턴스를 생성합니다.
+	 *
+	 * @param direction - 광원의 방향 벡터 [x, y, z]
+	 * @param color - 광원의 색상 (hex 문자열)
+	 * @param intensity - 광원의 세기
+	 */
 	constructor(direction: [number, number, number] = [-1, -1, -1], color: string = '#fff', intensity: number = 1) {
-		super(new ColorRGB(...convertHexToRgb(color, true)), intensity)
+		super(new ColorRGB(...convertHexToRgb(color, true)), intensity);
 		this.#directionX = direction[0];
 		this.#directionY = direction[1];
 		this.#directionZ = direction[2];
 	}
 
+	/** X 방향 벡터 값 반환 */
 	get directionX(): number {
 		return this.#directionX;
 	}
-
+	/** X 방향 벡터 값 설정 */
 	set directionX(value: number) {
 		this.#directionX = value;
 	}
 
+	/** Y 방향 벡터 값 반환 */
 	get directionY(): number {
 		return this.#directionY;
 	}
-
+	/** Y 방향 벡터 값 설정 */
 	set directionY(value: number) {
 		this.#directionY = value;
 	}
 
+	/** Z 방향 벡터 값 반환 */
 	get directionZ(): number {
 		return this.#directionZ;
 	}
-
+	/** Z 방향 벡터 값 설정 */
 	set directionZ(value: number) {
 		this.#directionZ = value;
 	}
 
+	/**
+	 * 전체 방향 벡터를 반환합니다.
+	 * @returns [x, y, z]
+	 */
 	get direction(): [number, number, number] {
 		return [this.#directionX, this.#directionY, this.#directionZ];
 	}
 
+	/**
+	 * 전체 방향 벡터를 설정합니다.
+	 * @param value - [x, y, z]
+	 */
 	set direction(value: [number, number, number]) {
 		this.#directionX = value[0];
 		this.#directionY = value[1];
@@ -51,5 +83,5 @@ class DirectionalLight extends BaseLight {
 	}
 }
 
-Object.freeze(DirectionalLight)
-export default DirectionalLight
+Object.freeze(DirectionalLight);
+export default DirectionalLight;
--- src/light/lights/PointLight.ts
+++ src/light/lights/PointLight.ts
@@ -2,62 +2,113 @@ import ColorRGB from "../../color/ColorRGB";
 import convertHexToRgb from "../../utils/convertColor/convertHexToRgb";
 import BaseLight from "../core/BaseLight";
 
+/**
+ * 점광원(PointLight)을 정의하는 클래스입니다.
+ *
+ * 이 광원은 특정 위치에서 모든 방향으로 빛을 방사하며,
+ * 반경(radius)을 기준으로 빛의 영향을 받는 범위를 설정할 수 있습니다.
+ *
+ * @example
+ * ```javascript
+ * const light = new RedGPU.Light.PointLight('#ffcc00', 1.5);
+ * light.setPosition(0, 5, 10);
+ * scene.lightManager.addPointLight(light);
+ * ```
+ * <iframe src="/RedGPU/examples/3d/light/pointLight/" width="100%" height="400" style="border:none;"></iframe>
+ */
 class PointLight extends BaseLight {
-	#radius: number = 1
-	#x: number = 0
-	#y: number = 0
-	#z: number = 0
+	/** 광원의 영향을 미치는 반경입니다. */
+	#radius: number = 1;
 
+	/** 광원의 X 좌표입니다. */
+	#x: number = 0;
+
+	/** 광원의 Y 좌표입니다. */
+	#y: number = 0;
+
+	/** 광원의 Z 좌표입니다. */
+	#z: number = 0;
+
+	/**
+	 * 새로운 PointLight 인스턴스를 생성합니다.
+	 *
+	 * @param color - 광원의 색상 (hex 문자열, 예: '#ffffff')
+	 * @param intensity - 광원의 세기 (기본값: 1)
+	 */
 	constructor(color: string = '#fff', intensity: number = 1) {
-		super(new ColorRGB(...convertHexToRgb(color, true)), intensity)
+		super(new ColorRGB(...convertHexToRgb(color, true)), intensity);
 	}
 
+	/** X 좌표를 반환합니다. */
 	get x(): number {
 		return this.#x;
 	}
 
+	/** X 좌표를 설정합니다. */
 	set x(value: number) {
 		this.#x = value;
 	}
 
+	/** Y 좌표를 반환합니다. */
 	get y(): number {
 		return this.#y;
 	}
 
+	/** Y 좌표를 설정합니다. */
 	set y(value: number) {
 		this.#y = value;
 	}
 
+	/** Z 좌표를 반환합니다. */
 	get z(): number {
 		return this.#z;
 	}
 
+	/** Z 좌표를 설정합니다. */
 	set z(value: number) {
 		this.#z = value;
 	}
 
+	/**
+	 * 광원의 위치를 [x, y, z] 형태로 반환합니다.
+	 */
 	get position(): [number, number, number] {
 		return [this.#x, this.#y, this.#z];
 	}
 
+	/**
+	 * 광원의 반경을 반환합니다.
+	 */
 	get radius(): number {
 		return this.#radius;
 	}
 
+	/**
+	 * 광원의 반경을 설정합니다.
+	 *
+	 * @param value - 반경 값 (예: 5.0)
+	 */
 	set radius(value: number) {
 		this.#radius = value;
 	}
 
+	/**
+	 * 광원의 위치를 설정합니다.
+	 *
+	 * @param x - X 좌표 또는 [x, y, z] 배열
+	 * @param y - Y 좌표 (x가 숫자일 경우)
+	 * @param z - Z 좌표 (x가 숫자일 경우)
+	 */
 	setPosition(x: number | [number, number, number], y?: number, z?: number) {
 		if (Array.isArray(x)) {
 			[this.#x, this.#y, this.#z] = x;
 		} else {
-			this.#x = x
-			this.#y = y
-			this.#z = z
+			this.#x = x;
+			this.#y = y;
+			this.#z = z;
 		}
 	}
 }
 
-Object.freeze(PointLight)
-export default PointLight
+Object.freeze(PointLight);
+export default PointLight;
