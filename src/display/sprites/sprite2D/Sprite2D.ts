fbe80bf14be9b5a8e927fd8144b7c4a768b4718c
webseon <webseon@gmail.com>
2025-09-28 15:15:47 +0900

    docs - Sprite3D, Sprite2D

--- src/display/sprites/sprite2D/Sprite2D.ts
+++ src/display/sprites/sprite2D/Sprite2D.ts
@@ -6,45 +6,108 @@ import consoleAndThrowError from "../../../utils/consoleAndThrowError";
 import {mixInMesh2D} from "../../mesh/core/mixInMesh2D";
 import Mesh from "../../mesh/Mesh";
 
+/** 2D 메시 기능이 믹스인된 베이스 클래스 */
 const BaseSprite2D = mixInMesh2D(Mesh);
 
+/**
+ * 2D 스프라이트 렌더링을 위한 클래스
+ *
+ * Sprite2D는 2D 게임이나 UI 요소를 렌더링하기 위한 클래스입니다.
+ * 평면 지오메트리를 기반으로 하며, 너비와 높이를 동적으로 조절할 수 있습니다
+ * <iframe src="/RedGPU/examples/2d/sprite2D/basic/"></iframe>
+ *
+
+ * 아래는 Sprite2D의 구조와 동작을 이해하는 데 도움이 되는 샘플 예제 목록입니다.
+ * @see [Hierarchy Sprite2D](/RedGPU/examples/2d/sprite2D/hierarchy/)
+ * @see [Pivot Sprite2D](/RedGPU/examples/2d/sprite2D/pivot/)
+ * @see [Child Methods](/RedGPU/examples/2d/sprite2D/childMethod/)
+ * @see [Mouse Event](/RedGPU/examples/2d/mouseEvent/sprite2D/)
+ */
 class Sprite2D extends BaseSprite2D {
+	/**
+	 * 스프라이트의 너비
+	 * @private
+	 */
 	#width: number = 1
+
+	/**
+	 * 스프라이트의 높이
+	 * @private
+	 */
 	#height: number = 1
 
+	/**
+	 * 새로운 Sprite2D 인스턴스를 생성합니다.
+	 *
+	 * @param redGPUContext - RedGPU 렌더링 컨텍스트
+	 * @param material - 스프라이트에 적용할 머티리얼 (옵션)
+	 */
 	constructor(redGPUContext: RedGPUContext, material?) {
 		super(redGPUContext, new Plane(redGPUContext, 1, 1, 1, 1, 1, true), material);
 		this.primitiveState.cullMode = GPU_CULL_MODE.FRONT
 	}
 
+	/**
+	 * 스프라이트의 너비를 반환합니다.
+	 * @returns 현재 너비 값
+	 */
 	get width(): number {
 		return this.#width;
 	}
 
+	/**
+	 * 스프라이트의 너비를 설정합니다.
+	 * @param value - 설정할 너비 값 (양수)
+	 * @throws {Error} 값이 양수가 아닌 경우
+	 */
 	set width(value: number) {
 		validatePositiveNumberRange(value)
 		this.#width = value;
 		this.dirtyTransform = true
 	}
 
+	/**
+	 * 스프라이트의 높이를 반환합니다.
+	 * @returns 현재 높이 값
+	 */
 	get height(): number {
 		return this.#height;
 	}
 
+	/**
+	 * 스프라이트의 높이를 설정합니다.
+	 * @param value - 설정할 높이 값 (양수)
+	 * @throws {Error} 값이 양수가 아닌 경우
+	 */
 	set height(value: number) {
 		validatePositiveNumberRange(value)
 		this.#height = value;
 		this.dirtyTransform = true
 	}
 
+	/**
+	 * 스프라이트의 머티리얼을 반환합니다.
+	 * @returns 현재 머티리얼
+	 */
 	get material() {
 		return this._material
 	}
 
+	/**
+	 * 머티리얼 설정을 시도합니다.
+	 * @param value - 설정하려는 머티리얼
+	 * @throws {Error} Sprite2D는 머티리얼을 변경할 수 없습니다
+	 */
 	set material(value) {
 		consoleAndThrowError('Sprite2D can not change material')
 	}
 
+	/**
+	 * 스프라이트의 크기를 설정합니다.
+	 *
+	 * @param width - 설정할 너비 값
+	 * @param height - 설정할 높이 값 (옵션, 생략 시 너비와 같은 값 사용)
+	 */
 	setSize(width: number, height?: number) {
 		this.width = width;
 		this.height = height !== undefined ? height : width;
@@ -52,4 +115,5 @@ class Sprite2D extends BaseSprite2D {
 }
 
 Object.freeze(Sprite2D)
+
 export default Sprite2D
--- src/display/sprites/sprite3D/Sprite3D.ts
+++ src/display/sprites/sprite3D/Sprite3D.ts
@@ -8,17 +8,48 @@ import parseWGSL from "../../../resources/wgslParser/parseWGSL";
 import Mesh from "../../mesh/Mesh";
 import vertexModuleSource from "../Sprite3D/shader/Sprite3DVertex.wgsl";
 
+/** Sprite3D 전용 버텍스 셰이더 모듈 이름 */
 const VERTEX_SHADER_MODULE_NAME = 'VERTEX_MODULE_SPRITE_3D'
+/** 파싱된 WGSL 셰이더 정보 */
 const SHADER_INFO = parseWGSL(vertexModuleSource);
+/** 버텍스 유니폼 구조체 정보 */
 const UNIFORM_STRUCT = SHADER_INFO.uniforms.vertexUniforms;
 
+/**
+ * Sprite3D의 빌보드 관련 속성을 정의하는 인터페이스
+ */
 interface Sprite3D {
+	/** 빌보드 원근감 적용 여부 */
 	useBillboardPerspective: boolean;
+	/** 빌보드 모드 사용 여부 */
 	useBillboard: boolean;
+	/**
+	 * 빌보드 고정 크기 배율
+	 *
+	 * useBillboardPerspective가 false일때만 적용됩니다.*/
 	billboardFixedScale: number;
 }
 
+/**
+ * 3D 공간에서 항상 카메라를 향하는 2D 스프라이트 객체
+ *
+ * Sprite3D는 Mesh 클래스를 상속받아 빌보드 기능을 제공하는 클래스입니다.
+ * 빌보드는 3D 공간에 배치되지만 항상 카메라 방향을 바라보는 평면 객체로,
+ * UI 요소, 파티클, 텍스트, 아이콘 등을 3D 씬에 표시할 때 유용합니다.
+ *
+ * <iframe src="/RedGPU/examples/3d/sprite/sprite3D/"></iframe>
+ *
+ * 아래는 Sprite3D의 구조와 동작을 이해하는 데 도움이 되는 샘플 예제 목록입니다.
+ * @see [Mouse Event](/RedGPU/examples/3d/mouseEvent/sprite3D/)
+ */
 class Sprite3D extends Mesh {
+	/**
+	 * 새로운 Sprite3D 인스턴스를 생성합니다.
+	 *
+	 * @param redGPUContext - RedGPU 렌더링 컨텍스트
+	 * @param material - 스프라이트에 적용할 머티리얼 (옵션)
+	 * @param geometry - 스프라이트의 지오메트리 (기본값: 새로운 Plane 인스턴스)
+	 */
 	constructor(redGPUContext: RedGPUContext, material?, geometry?: Geometry | Primitive,) {
 		super(redGPUContext);
 		this._geometry = geometry || new Plane(redGPUContext);
@@ -29,15 +60,30 @@ class Sprite3D extends Mesh {
 		this.primitiveState.cullMode = GPU_CULL_MODE.NONE
 	}
 
+	/**
+	 * Sprite3D 전용 커스텀 버텍스 셰이더 모듈을 생성합니다.
+	 *
+	 * 이 메서드는 빌보드 기능을 지원하는 전용 버텍스 셰이더를 생성합니다.
+	 * 일반 메시와 달리 카메라 방향에 따라 정점 위치를 동적으로 계산하는
+	 * 셰이더 로직이 포함되어 있습니다.
+	 *
+	 * @returns 생성된 버텍스 셰이더 모듈 정보
+	 *
+	 */
 	createCustomMeshVertexShaderModule() {
 		return this.createMeshVertexShaderModuleBASIC(VERTEX_SHADER_MODULE_NAME, SHADER_INFO, UNIFORM_STRUCT, vertexModuleSource)
 	}
 }
 
+/**
+ * Sprite3D 클래스에 빌보드 관련 속성들을 정의합니다.
+ */
 DefineForVertex.defineByPreset(Sprite3D, [
 	[DefineForVertex.PRESET_BOOLEAN.USE_BILLBOARD_PERSPECTIVE, true],
 	[DefineForVertex.PRESET_BOOLEAN.USE_BILLBOARD, true],
 	[DefineForVertex.PRESET_POSITIVE_NUMBER.BILLBOARD_FIXED_SCALE, 0.1, 0.1],
 ])
+
 Object.freeze(Sprite3D)
+
 export default Sprite3D
