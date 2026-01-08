ca5fd67463726f1ee7127239c46108817efe70ef
webseon <webseon@gmail.com>
2026-01-08 13:38:25 +0900

    예제 및 스타일 리소스 경로 정리 및 CSS 스타일 수정

--- examples/postEffect/ssao/index.js
+++ examples/postEffect/ssao/index.js
@@ -52,7 +52,7 @@ function loadGLTF(view, url) {
 
 const renderTestPane = async (redGPUContext, targetView) => {
     const {Pane} = await import('https://cdn.jsdelivr.net/npm/tweakpane@4.0.3/dist/tweakpane.min.js');
-    const {createIblHelper, setDebugButtons} = await import('../../exampleHelper/createExample/panes');
+    const {createIblHelper, setDebugButtons} = await import('../../exampleHelper/createExample/panes/index.js');
     setDebugButtons(redGPUContext);
     const pane = new Pane();
     createIblHelper(pane, targetView, RedGPU);
