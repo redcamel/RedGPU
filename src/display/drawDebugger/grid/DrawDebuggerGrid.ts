4daa93cce1cc1d95ac2e359814c4952ee8fd9aa6
redcamel <webseon@gmail.com>
2026-01-12 02:00:39 +0900

    그리드 셰이더 거리 기반 Fade 최소값 설정 및 알파 블렌드 로직 수정

--- src/display/drawDebugger/grid/DrawDebuggerGrid.ts
+++ src/display/drawDebugger/grid/DrawDebuggerGrid.ts
@@ -55,7 +55,7 @@ class DrawDebuggerGrid {
         const shaderModule: GPUShaderModule = resourceManager.createGPUShaderModule(SHADER_MODULE_NAME, moduleDescriptor)
         this.#blendColorState = new BlendState(this, GPU_BLEND_FACTOR.SRC_ALPHA, GPU_BLEND_FACTOR.ONE_MINUS_SRC_ALPHA, GPU_BLEND_OPERATION.ADD)
         this.#blendAlphaState = new BlendState(this, GPU_BLEND_FACTOR.SRC_ALPHA, GPU_BLEND_FACTOR.ONE_MINUS_SRC_ALPHA, GPU_BLEND_OPERATION.ADD)
-        this.#lineColor = new ColorRGBA(128, 128, 128, 0.5)
+        this.#lineColor = new ColorRGBA(128, 128, 128, 0.25)
         const vertexBindGroupLayout = resourceManager.getGPUBindGroupLayout(ResourceManager.PRESET_GPUBindGroupLayout_System)
         const fragmentBindGroupLayout = redGPUContext.resourceManager.getGPUBindGroupLayout('GRID_MATERIAL_BIND_GROUP_LAYOUT') || redGPUContext.resourceManager.createBindGroupLayout(
             'GRID_MATERIAL_BIND_GROUP_LAYOUT',
