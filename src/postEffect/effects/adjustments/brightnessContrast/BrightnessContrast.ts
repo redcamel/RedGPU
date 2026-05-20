bdf563124b3fabac7e47c8270da8c3e7564b1165
webseon <webseon@gmail.com>
2026-05-20 09:29:12 +0900

    BrightnessContrast 효과 리팩토링: DefineUniformProperty 적용 및 기존 속성 정의 코드 제거 - V4.1.0-Alpha-rnd

--- src/postEffect/effects/adjustments/brightnessContrast/BrightnessContrast.ts
+++ src/postEffect/effects/adjustments/brightnessContrast/BrightnessContrast.ts
@@ -1,10 +1,14 @@
 import RedGPUContext from "../../../../context/RedGPUContext";
-import validateNumberRange from "../../../../runtimeChecker/validateFunc/validateNumberRange";
 import ASinglePassPostEffect from "../../../core/ASinglePassPostEffect";
 import createBasicPostEffectCode from "../../../core/createBasicPostEffectCode";
 import computeCode from "./wgsl/computeCode.wgsl"
 import uniformStructCode from "./wgsl/uniformStructCode.wgsl"
+import DefineUniformProperty from "../../../../defineProperty/DefineUniformProperty";
 
+interface BrightnessContrast{
+    brightness:number,
+    contrast:number
+}
 /**
  * [KO] 밝기/대비 조절 후처리 이펙트입니다.
  * [EN] Brightness/Contrast adjustment post-processing effect.
@@ -21,19 +25,6 @@ import uniformStructCode from "./wgsl/uniformStructCode.wgsl"
  */
 class BrightnessContrast extends ASinglePassPostEffect {
     /**
-     * [KO] 밝기 (-150 ~ 150)
-     * [EN] Brightness (-150 ~ 150)
-     * @defaultValue 0
-     */
-    #brightness: number = 0
-    /**
-     * [KO] 대비 (-50 ~ 100)
-     * [EN] Contrast (-50 ~ 100)
-     * @defaultValue 0
-     */
-    #contrast: number = 0
-
-    /**
      * [KO] BrightnessContrast 인스턴스를 생성합니다.
      * [EN] Creates a BrightnessContrast instance.
      *
@@ -49,43 +40,14 @@ class BrightnessContrast extends ASinglePassPostEffect {
             createBasicPostEffectCode(this, computeCode, uniformStructCode)
         );
     }
-
-    /**
-     * [KO] 밝기 값을 반환합니다.
-     * [EN] Returns the brightness value.
-     */
-    get brightness(): number {
-        return this.#brightness;
-    }
-
-    /**
-     * [KO] 밝기 값을 설정합니다. (-150 ~ 150)
-     * [EN] Sets the brightness value. (-150 ~ 150)
-     */
-    set brightness(value: number) {
-        validateNumberRange(value, -150, 150)
-        this.#brightness = value;
-        this.updateUniform('brightness', value)
-    }
-
-    /**
-     * [KO] 대비 값을 반환합니다.
-     * [EN] Returns the contrast value.
-     */
-    get contrast(): number {
-        return this.#contrast;
-    }
-
-    /**
-     * [KO] 대비 값을 설정합니다. (-50 ~ 100)
-     * [EN] Sets the contrast value. (-50 ~ 100)
-     */
-    set contrast(value: number) {
-        validateNumberRange(value, -50, 100)
-        this.#contrast = value;
-        this.updateUniform('contrast', value)
-    }
 }
+
+DefineUniformProperty.defineNumber(BrightnessContrast,
+    [
+        {key: 'brightness', value: 0, min: -150, max: 150},
+        {key: 'contrast', value: 0, min: -50, max: 100}
+    ]
+)
 
 Object.freeze(BrightnessContrast)
 export default BrightnessContrast
