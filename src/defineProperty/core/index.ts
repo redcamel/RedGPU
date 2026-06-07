f9bc6ac406d1b4ec699462ba6cc749f62c1d73c9
webseon <webseon@gmail.com>
2026-06-07 14:47:32 +0900

    DefineGPUProperty 모듈에서 core 디렉토리 및 관련 export 제거 (V4.1.0-Alpha-rnd)

--- src/defineProperty/core/index.ts
+++
@@ -1,6 +0,0 @@
-import applyProperties from "./applyProperties";
-import defineProperty_SETTING from "./defineProperty_SETTING";
-import defineVector from "./defineVector";
-import updateTargetUniform from "./updateTargetUniform";
-
-export {applyProperties, defineProperty_SETTING, defineVector, updateTargetUniform};
