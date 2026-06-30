1368cfc171f108adae6de3c0a4e58e3ada960b06
webseon <webseon@gmail.com>
2026-06-30 15:45:51 +0900

    #730 - 스키닝 메쉬 및 조인트 처리 로직 개선: - `isJointMesh` 제거 및 메쉬 상태 관리 단순화. - 조인트 메쉬에 공통 지오메트리 및 머티리얼 참조 추가. - 스킨 정보 클래스에 조인트용 `geometry` 및 `material` 초기화 로직 추가. (V4.2.0-Alpha-rnd)

--- src/loader/gltf/parsers/animation/parseJoint_GLTF.ts
+++ src/loader/gltf/parsers/animation/parseJoint_GLTF.ts
@@ -8,8 +8,8 @@ const parseJoint_GLTF = (gltfLoader: GLTFLoader, skinInfo: ParsedSkinInfo_GLTF, 
     if (jointMesh) {
 
         skinInfo.joints.push(jointMesh);
-        jointMesh.geometry = skinInfo.geometry
-        jointMesh.material = skinInfo.material
+        // jointMesh.geometry = skinInfo.geometry
+        // jointMesh.material = skinInfo.material
         // jointMesh.depthStencilState.depthCompare = GPU_COMPARE_FUNCTION.ALWAYS
         // jointMesh.primitiveState.topology = GPU_PRIMITIVE_TOPOLOGY.LINE_LIST
     } else {
