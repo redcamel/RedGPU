31acd318f1dce4a137e7344572584df39ea628fa
webseon <webseon@gmail.com>
2026-07-16 13:44:02 +0900

    클러스터 조명 렌더링 설정 개선: Z축 타일 개수와 클러스터당 최대 조명 개수를 각각 64로 확장 (V4.3.0-Alpha-rnd)

--- src/light/clusterLight/core/PassClustersLightHelper.ts
+++ src/light/clusterLight/core/PassClustersLightHelper.ts
@@ -21,7 +21,7 @@ const PassClustersLightHelper = {
      * [KO] Z축 타일 개수
      * [EN] Number of tiles on the Z-axis
      */
-    TILE_COUNT_Z: 24,
+    TILE_COUNT_Z: 64,
     /**
      * [KO] X축 워크그룹 크기
      * [EN] Workgroup size on the X-axis
@@ -41,7 +41,7 @@ const PassClustersLightHelper = {
      * [KO] 클러스터당 최대 조명 개수
      * [EN] Maximum number of lights per cluster
      */
-    MAX_LIGHTS_PER_CLUSTER: 32,
+    MAX_LIGHTS_PER_CLUSTER: 64,
     /**
      * [KO] 최대 클러스터 조명 개수
      * [EN] Maximum number of cluster lights
